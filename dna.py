afaf21984149f0c2c9df0996627ab9ca3f1b8ef6
kamilbednarski <64035847+kamilbednarski@users.noreply.github.com>
2020-11-04 12:29:35 +0100

    Add files via upload

--- dna.py
+++ dna.py
@@ -11,103 +11,54 @@ if len(argv) != 3:
 
 # Open database file. Check number of columns. Save first line of file.
 with open(argv[1]) as database_file:
-    database_reader = csv.reader(database_file, delimiter = ",")
+    database_reader = csv.reader(database_file, delimiter=",")
     first_line = next(database_reader)
     number_of_columns = len(first_line)
 
 
 # Save sequence types from first line of file.
-dictionary_size = number_of_columns - 1
 dna_sequences = {}
-i = 0
-while i < dictionary_size:
-    dna_sequences[i] = first_line[i + 1]
-    i += 1
+for i in range(1, len(first_line)):
+    dna_sequences[first_line[i]] = 0
 
 
-# Open file with DNA sample. Save sequence for comparison in memeowy.
+# Open file with DNA sample. Save sample for comparison in memory.
 with open(argv[2]) as dna_file:
     dna_sample = dna_file.read()
-    
-    
-print(dna_sequences)
-
-for key in dna_sequences:
-    print(f"key: {dna_sequences[key]}")
 
 
+# Count repetitions of each sequence in DNA sample.
+counter = 0
+# For each sequence:
+for sequence in dna_sequences:
+    i = 1
+    # While found sequence of repetition times 'i'(if methon 'find' result is -1, than its not there):
+    while dna_sample.find(sequence * i) != -1:
+        counter = i
+        i += 1
+    # At the end save highest number of repetitions for each sequence:
+    dna_sequences[sequence] = counter
 
 
+# Open database file. Save first line of file.
+with open(argv[1]) as database_file:
+    database_reader = csv.reader(database_file, delimiter=",")
+    line = next(database_reader)
+    number_of_columns = len(line)
 
-#temp_counter = 0
-#main_counter = 0
-#start_index = 0
-#end_index = start_index + len(dna_sequences[i])
+    # Save sequence types from first line of file.
+    temporary_sequences = {}
+    for i in range(1, len(line)):
+        temporary_sequences[line[i]] = 0
 
-#for i in dna_sequences:
- #   if
-
-
-
-
-
-
-
-
-dna_part = dna_sequences[i]
-start_index = 0
-end_index = start_index + len(dna_part)
-temporary_counter = 0
-main_counter = 0
-
-
-if dna_sample[start_index, end_index] == dna_part:
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Check if input is valid (i.e. if there are 3 arguments); if not, print error message.
-# Read the DNA sequence from the file
-# Store the information in a string
-# Create a dictionary to store the DNA sequences that are to be counted
-# Extract the sequences from the database into a list
-# Copy the list into a dictionary where the genes are the keys
-# Iterate through the DNA sequence
-# Add a counter — if repetitions of the values from sequence dictionaries are found, add them up
-# Open the database (of people) file
-# Iterate through and compare to the sequences dictionary
-# If there is a match, print the name of the person; if not, no match.
+    # Search every row of database file.
+    for row in database_reader:
+        i = 1
+        for sequence in temporary_sequences:
+            temporary_sequences[sequence] = int(row[i])
+            i += 1
+        # If found exact same dictionary, print name and stop the program.
+        if dna_sequences == temporary_sequences:
+            print(row[0])
+            exit()
+    print("No match")
